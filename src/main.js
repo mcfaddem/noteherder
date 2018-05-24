d821e4cfa246756f327e5a1ef09732de62e7f08f
Madeline McFadden <mcfaddem@iu.edu>
2018-05-24 10:04:35 -0400

    update main.js

--- src/main.js
+++ src/main.js
@@ -13,6 +13,13 @@ class Main extends React.Component {
     }
   }
 
+  componentWillMount() {
+    const notes = JSON.parse(window.localStorage.getItem('notes'))
+    if (notes) {
+      this.setState({ notes })
+    }
+  }
+
   blankNote = () => {
     return {
       id: null,
@@ -44,6 +51,21 @@ class Main extends React.Component {
 
     this.setState({ notes })
     this.setCurrentNote(note)
+
+    window.localStorage.setItem('notes', JSON.stringify(notes))
+  }
+
+  removeCurrentNote = () => {
+    const notes = [...this.state.notes]
+
+    const i = notes.findIndex(note => note.id === this.state.currentNote.id)
+    if (i > -1) {
+      notes.splice(i, 1)
+      this.setState({ notes })
+      window.localStorage.setItem('notes', JSON.stringify(notes))
+    }
+
+    this.resetCurrentNote()
   }
 
   render() {
@@ -57,6 +79,7 @@ class Main extends React.Component {
         <NoteForm
           currentNote={this.state.currentNote}
           saveNote={this.saveNote}
+          removeCurrentNote={this.removeCurrentNote}
         />
       </div>
     )
